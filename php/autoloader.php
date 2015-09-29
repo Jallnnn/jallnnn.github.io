c64f7d9186b998135c4651433d0be67c58cd7b2f
Jallnnn <julle81@hotmail.com>
2015-09-29 11:47:27 +0200

    huhu

--- php/autoloader.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-function __autoload($class_name) {
-	include "classes/".strtolower($class_name).'.class.php';
-}
--- php/get_menu_content.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-include_once ("autoloader.php");
-
-$cq = New ContentQueries("127.0.0.1","supercar_kiddos","root","mysql");
-
-
-	echo(json_encode($cq->getMenuLinks($_REQUEST["get_menu_links"])));
